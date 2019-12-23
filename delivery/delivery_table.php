b9abe6befc748774e228541e6236c0bb26580dc3
trot505 <trot505@yandex.ru>
2019-12-23 15:33:29 +0400

    delivery 231219

--- delivery/delivery_table.php
+++ delivery/delivery_table.php
@@ -1,7 +1,67 @@
-<?
+<?php
 include_once "../fun.php";
 $file_str = file_get_contents("./delivery_menu.csv");
 $arr_string_menu = explode(PHP_EOL,$file_str);
-pp($arr_string_menu);
+$arr_chapter = [];
 
+foreach ($arr_string_menu as $key => $string_el) {
+    $arr_menu_el = explode('_',$string_el);
+    $arr_chapter[$arr_menu_el[0]][] = [
+        'name' => $arr_menu_el[1],
+        'images' => $arr_menu_el[2],
+        'property' => $arr_menu_el[3],
+        'price' => $arr_menu_el[4],
+    ];
+}
+//pp($arr_chapter);
+
+$delivery_menu = '<div class="delivery_menu">';
+$delivery_page = '<div class="delivery_page">';
+$count_tab = 0;
+foreach ($arr_chapter as $name_menu => $arr_value) {
+    if (empty($arr_value)) continue;
+    $active_style = ($count_tab == 0)?'active_tab':'';
+    $delivery_menu .= '<div class="delivery_menu_item" dtata-id-tab="delivery_tab_'.$count_tab.'">'. $name_menu.'</div>';
+    $delivery_page .= '<div id="delivery_product_list delivery_tab_'.$count_tab.' '.$active_style.'">';
+    foreach ($arr_value as $val_product) {
+        $delivery_page .= '<div class="product_card">';
+        $name; $images; $property; $price;
+        foreach ($val_product as $name_options => $v) {
+            switch ($name_options){            
+                case 'name':
+                    $name = '<h3>'.$v.'</h3>';
+                    break;
+                case 'images':
+                    $images = '<img src="'.$v.'"/>';
+                    break;
+                case 'property':
+                    $property = '<div class="product_property">'.$v.'</div>';
+                    break;
+                case 'price':
+                    $price = '<div class="product_price">'.$v.'</div>';
+                    break;
+                default:
+                    break;
+            }
+        }
+        $delivery_page .= $name.$images.$property.$price.'</div>';
+    }    
+    $delivery_page .= '</div>';
+    $count_tab++;
+}
+$delivery_menu .= '</div>';
+$delivery_page .= '</div>';
 ?>
+
+<div id="#delivery_page">
+    <div class="delivery_contact"></div>
+    <div class="delivery_content">
+        <div class="delivery_selection">
+            <?=$delivery_menu?>
+            <?=$delivery_page?>
+        </div>
+        <div class="delivery_cart">
+
+        </div>
+    </div>
+</div>
