68ef300c9c4c3e3d0100a0acc53401e2223b5122
trot505 <trot505@yandex.ru>
2020-01-23 13:02:50 +0400

    work 23012020

---
+++ index.php
@@ -0,0 +1,177 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Краоке бар Горластый Гарри">
+<link rel="shortcut icon" href="/favicon.ico">
+<title>Горластый Гарри</title>
+<!-- CSS -->
+<link href="css/style.css" rel="stylesheet">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">-->
+<!--<script src="https://kit.fontawesome.com/2cc185786d.js" crossorigin="anonymous"></script>-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" integrity="sha384-REHJTs1r2ErKBuJB0fCK99gCYsVjwxHrSU0N7I1zl9vZbggVJXRMsv/sLlOAGb4M" crossorigin="anonymous">
+
+
+</head>
+<body class="frontpage">
+
+<div id="loader-wrapper">
+<div id="loader"></div>
+<div class="loader-section section-left"></div>
+<div class="loader-section section-right"></div>
+</div>
+
+<section id="back_content"></section>
+
+<div class="fixedcallicon">
+	<i class="fa fa-phone"></i><span class="hide"><a href="tel:+73412775095">+7 (3412) 775-095</a></span>
+</div>
+<div class="container">
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="intro">
+			<h1><span class="smaller">Ижевск, пер. Интернациональный, 3</span>
+			<span class="small">- Пой со мной -</span><br/>
+			</h1>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="about">
+			<div class="aboutbadge">
+				<span><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><br/>
+				<span class="border">Awarded Salon</span><br/>
+				Est. 2007</span>
+			</div>
+			<div class="aboutbadge black">
+				<span>Upbeat, chic, and funky salon. We are based on twenty-five years of experience. We specialize in couture cuts and blowouts, men's cuts, natural and funky hair colors, and highlights. </span>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="voucher">
+			<div class="voucher-whitetransparent">
+				<h2><i class="fa fa-gift"></i><br/>REDEEM YOUR VOUCHER<br/>20% OFF IN 2016<br/><a href="#" class="menu_see">Посмотреть меню</a></h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="pricingbadge">
+			<h4>High Quality, Low Prices</h4>
+			<ul class="pricing_menu__list">
+				<li class="pricing_menu__row">
+				<div class="pricing_menu__meal">
+					<span>Haircut & Blowdry</span>
+				</div>
+				<span class="pricing_menu__price">$32.00</span>
+				</li>
+				<li class="pricing_menu__row">
+				<div class="pricing_menu__meal">
+					<span>Express Cut</span>
+				</div>
+				<span class="pricing_menu__price">$22.00</span>
+				</li>
+				<li class="pricing_menu__row">
+				<div class="pricing_menu__meal">
+					<span>Shampoo & Set</span>
+				</div>
+				<span class="pricing_menu__price">$19.00</span>
+				</li>
+				<li class="pricing_menu__row">
+				<div class="pricing_menu__meal">
+					<span>Bleach & Tone</span>
+				</div>
+				<span class="pricing_menu__price">$90.00</span>
+				</li>
+				<li class="pricing_menu__row">
+				<div class="pricing_menu__meal">
+					<span>Foil with Haircut</span>
+				</div>
+				<span class="pricing_menu__price">$43.00</span>
+				</li>
+				<a class="fullpricelist" href="#"><i class="fa fa-file-pdf-o"></i> View our full pricing list</a>
+			</ul>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="testimonialarea">
+			<div class="testimonialarea-bubble">
+				<div class="testimonial-widget">
+					<h3 class="uppercase">BEAUTTIO MADE ME BEAUTIFUL</h3>
+					<div class="testimonial">
+						<p>
+							 "They took care of my wedding, everyone asked me who was my stylist. Great prices, great staff!"
+						</p>
+						<p>
+							<strong>Cindy, New York</strong>
+						</p>
+					</div>
+					<div class="testimonial">
+						<p>
+							 "I am a regular customer of their services, once I pay them a visit I am a different person."
+						</p>
+						<p>
+							<strong>Layana, Munchen</strong>
+						</p>
+					</div>
+					<div class="testimonial">
+						<p>
+							 "Thank you so much, Beauttio, you have one of the best hairstylists aroung, I give it 5 <i class="fa fa-star"></i>."
+						</p>
+						<p>
+							<strong>Jane, London</strong>
+						</p>
+					</div>
+					<button class="prev-testimonial">Prev</button>
+					<button class="next-testimonial">Next</button>
+				</div>
+			</div>
+			<div class="contactform-bubble">
+				<form autocomplete="off" class="contactform" method="post" action="contact.php" id="contactform">
+					<input name="name" type="text" placeholder="Name">
+					<input name="email" type="text" placeholder="E-mail">
+					<textarea name="comment" placeholder="Message" rows="4"></textarea>
+					<input value="SEND" type="submit" id="submit" class="btnsend">
+					<div class="done">
+						<div class="alert-box success">
+							<i>Message sent! We'll answer shortly.</i>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="contactaddress-bubble">
+				<div class="contactaddress">
+					 1044 Madison Avenue, New York, NY 10075, US<br/>
+					<a class="map" href="#">Map</a>
+				</div>
+			</div>
+		</div>
+	
+</div>
+
+
+	</section>
+</div>
+
+
+<!-- Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<script src="js/common.js"></script>
+<script src="js/home.js"></script>
+<script src="js/testimonials.js"></script>
+
+</body>
+</html>
